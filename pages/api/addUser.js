dfbda1bbdf995fa12a00fdbc44f4279cc3422742
johnchmura <113478688+johnchmura@users.noreply.github.com>
2023-11-03 00:19:14 -0500

    db and initialization

---
+++ pages/api/addUser.js
@@ -0,0 +1,19 @@
+// pages/api/addUser.js
+import pool from '../../db';
+
+export default async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const client = await pool.connect();
+    const query = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *';
+    const result = await client.query(query, [username, email, password]);
+    const insertedUser = result.rows[0];
+    client.release();
+
+    res.status(200).json(insertedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
