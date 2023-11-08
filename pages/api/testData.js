865490b768948951ca256e2c36d0d41202ada6d1
johnchmura <113478688+johnchmura@users.noreply.github.com>
2023-11-07 21:14:42 -0600

    please work

---
+++ pages/api/testData.js
@@ -0,0 +1,20 @@
+// pages/api/testData.js
+import pool from "@/lib/db";
+
+export default async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT username FROM users');
+    const data = result.rows;
+    client.release();
+
+    if (data.length === 0) {
+      console.log('No data found in the users table.');
+    }
+
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
